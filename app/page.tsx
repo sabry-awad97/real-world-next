93846b4cc3958072c2f40c37c1b6be61e8a2c384
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-13 12:19:05 +0200

    🚀 feat(page.tsx): Add user listing functionality with axios integration 📦 chore(package.json): Add new dependencies for form handling and API requests

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
+import axios from 'axios';
+import Link from 'next/link';
+
+interface User {
+  id: string;
+  name: string;
+}
+
 const Home = async () => {
-  return <div>This is the home page.</div>;
+  const { data: users } = await axios.get<User[]>(
+    'http://localhost:3030/users'
+  );
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>
+          <Link href={`/users/${user.id}`}>{user.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Home;
