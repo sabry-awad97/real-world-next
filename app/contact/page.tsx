fd8420fa01ac87b2b8317f15f13be3384ede3829
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-13 02:32:19 +0200

    ✨ add(contact): Create ContactPage component

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+const ContactPage = () => {
+  return (
+    <div>
+      <ul>
+        <li>Email: myemail@example.com</li>
+        <li>Twitter: @myusername </li>
+        <li>Instagram: myusername </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContactPage;
