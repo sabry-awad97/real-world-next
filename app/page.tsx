1e12d48283a55909492dc35bb667d6aa5cbc5cd4
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-13 03:31:06 +0200

    🎨 style(page.tsx): Add image component for better UI 🔧 config(next.config.js): Configure Next.js to handle remote images from Unsplash

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { getUserName } from './lib/user-info';
+import Image from 'next/image';
 
 const Home = () => {
   const user = getUserName();
@@ -8,6 +9,13 @@ const Home = () => {
         <h1 className="text-3xl font-bold mb-4 text-gray-800">
           Welcome, {user}!
         </h1>
+        <Image
+          src="https://images.unsplash.com/photo-1605460375648-278bcbd579a6"
+          width={500}
+          height={200}
+          alt="A beautiful English Setter"
+          objectFit="cover"
+        />
       </div>
     </div>
   );
