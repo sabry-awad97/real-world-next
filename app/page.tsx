7f4da0b20128b5e93af4b25a8d1d8640b2ac65b5
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-14 12:50:35 +0200

    🔧 refactor(README.md): Update project description in README ✨ feat(app): Integrate Chakra UI components in Home page 🌐 chore(app): Add Chakra UI theme providers 🔄 chore(package.json): Update dependencies and devDependencies 🗑️ remove(tailwind.config.ts): Remove Tailwind CSS configuration file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button, VStack } from '@chakra-ui/react';
+
 const Home = async () => {
-  return <div>This is the home page.</div>;
+  return (
+    <VStack padding="10">
+      <Button backgroundColor="brand.100">brand.100</Button>
+      <Button backgroundColor="brand.200">brand.200</Button>
+      <Button backgroundColor="brand.300">brand.300</Button>
+      <Button backgroundColor="brand.400">brand.400</Button>
+    </VStack>
+  );
 };
 
 export default Home;
---
+++ app/providers.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: '#ffebee',
+      200: '#e57373',
+      300: '#f44336',
+      400: '#e53935',
+    },
+  },
+});
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
