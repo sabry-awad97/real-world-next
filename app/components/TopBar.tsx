94b56eefa64af8043209d6e0401cf5ed3750fa86
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-14 17:08:28 +0200

    ✨ add(TopBar.tsx): Implement TopBar component for navigation and theme toggle

---
+++ app/components/TopBar.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Link } from '@chakra-ui/next-js';
+import {
+  Box,
+  Button,
+  Flex,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+function TopBar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const ColorModeIcon = colorMode === 'light' ? SunIcon : MoonIcon;
+
+  return (
+    <Box
+      width="100%"
+      padding="1"
+      backgroundColor={useColorModeValue('gray.50', 'gray.700')}
+    >
+      <Box maxWidth="container.xl" margin="auto">
+        <Flex justifyContent="space-between" alignItems="center">
+          <Box maxWidth="container.xl">
+            <Link href="/">
+              <Button size="xs" borderRadius="sm">
+                Home
+              </Button>
+            </Link>
+            <Link href="/users">
+              <Button size="xs" marginLeft="2" borderRadius="sm">
+                Users
+              </Button>
+            </Link>
+          </Box>
+
+          <Button
+            aria-label="UI Theme"
+            leftIcon={<ColorModeIcon />}
+            onClick={toggleColorMode}
+            size="xs"
+            borderRadius="sm"
+          >
+            Toggle theme
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+}
+
+export default TopBar;
